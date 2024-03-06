cbb12ff143409851ed9b9212446fe745287a714c
Christopher Oliver <coliver650@gmail.com>
2024-03-06 13:29:55 -0500

    save morning work

--- app/ui/education/table.tsx
+++ app/ui/education/table.tsx
@@ -2,25 +2,21 @@ import me from '@/app/data/me.json'
 
 export default function EducationTable() {
 
-    const educationList = me.education;
-
-    return (
-        <div className='container mx-auto'>
-            {educationList?.map((education) => (
-                <>
-                    <hr></hr>
-                    <div className='mb-10' key={ education.id }>
-                        <div className='md:flex md:flex-row my-4 text-2xl'>
-                            <div className='basis-1/2'>{ education.degree }</div>
-                            <div className='basis-1/2'>{ education.name }</div>
-                        </div>
-                        <div className='md:flex md:flex-row mb-2 text-xl'>
-                            <div className='basis-1/2 mb-2'>{ education.datesAttended }</div>
-                            <div className='basis-1/2'>{ education.aboutDegree }</div>
-                        </div>
-                    </div>
-                </>
-            ))}
-        </div>
+    const schools = me.education?.map(education =>
+        <li key={ education.id }>
+            <hr></hr>
+            <div className='mb-10'>
+                <div className='md:flex md:flex-row my-4 text-2xl'>
+                    <div className='basis-1/2'>{ education.degree }</div>
+                    <div className='basis-1/2'>{ education.name }</div>
+                </div>
+                <div className='md:flex md:flex-row mb-2 text-xl'>
+                    <div className='basis-1/2 mb-2'>{ education.datesAttended }</div>
+                    <div className='basis-1/2'>{ education.aboutDegree }</div>
+                </div>
+            </div>
+        </li>
     );
+
+    return <ul className='container mx-auto'>{ schools }</ul>
 }
--- app/ui/experience/table.tsx
+++ app/ui/experience/table.tsx
@@ -2,31 +2,27 @@ import me from '@/app/data/me.json'
 
 export default function ExperienceTable() {
 
-    const experienceList = me.experience;
-
-    return (
-        <div className='container mx-auto'>
-            {experienceList?.map((experience) => (
-                <>
-                    <hr></hr>
-                    <div className='mb-10' key={ experience.id }>
-                        <div className='md:flex md:flex-row my-4 text-2xl'>
-                            <div className='basis-1/2'>{ experience.title }</div>
-                            <div className='basis-1/2'>{ experience.company }</div>
-                        </div>
-                        <div className='md:flex md:flex-row mb-2 text-xl'>
-                            <div className='basis-1/2'>{ experience.role }</div>
-                            <div className='basis-1/2'>{ experience.datesWorked }</div>
-                        </div>
-                        <div>
-                            {experience.accomplishments?.map((accomplishment) => (
-                                <div className='mb-2'>{ accomplishment.description }</div>
-                            ))}
-                            <div className='mt-4'>{ experience.skills }</div>
-                        </div>
-                    </div>
-                </>
-            ))}
-        </div>
+    const experiences = me.experience?.map(experience =>
+        <li key={ experience.id }>
+            <hr></hr>
+            <div className='mb-10'>
+                <div className='md:flex md:flex-row my-4 text-2xl'>
+                    <div className='basis-1/2'>{ experience.title }</div>
+                    <div className='basis-1/2'>{ experience.company }</div>
+                </div>
+                <div className='md:flex md:flex-row mb-2 text-xl'>
+                    <div className='basis-1/2'>{ experience.role }</div>
+                    <div className='basis-1/2'>{ experience.datesWorked }</div>
+                </div>
+                <div>
+                    {experience.accomplishments?.map((accomplishment) => (
+                        <div className='mb-2'>{ accomplishment.description }</div>
+                    ))}
+                    <div className='mt-4'>{ experience.skills }</div>
+                </div>
+            </div>
+        </li>
     );
+
+    return <ul className='container mx-auto'>{ experiences }</ul>
 }
